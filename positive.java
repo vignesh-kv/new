486fff18b5c0c6bc5f2499e13fa58e759c048ef3
vignesh-kv <kv.vignesh2004@gmail.com>
2025-01-28 22:23:21 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,13 @@
+class positive{
+public static void main(String[]args){
+System.out.println("Enter the number:");
+int a=15;
+int b=21;
+if(a>b)
+System.out.print("positive");
+else if(a==0)
+System.out.print("error");
+else
+System.out.print("negative");
+}
+}
