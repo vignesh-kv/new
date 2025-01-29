45244a7e57e51ae97a87c84eb2f87ea4f292b695
vignesh-kv <kv.vignesh2004@gmail.com>
2025-01-29 19:17:22 +0530

    Add files via upload

---
+++ specialtool.java
@@ -0,0 +1,14 @@
+class specialtool{
+public static void main(String[]args){
+System.out.println("enter the number:");
+int a=15;
+int b=20;
+if(a<b && a<=b){
+System.out.println(true);
+}if(a>b && a>=b){
+System.out.println(false);
+}if(a!=b){
+System.out.println(true);
+}
+}
+}
