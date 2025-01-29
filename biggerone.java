e21a149d8074db0dbd0974ef8145bb30af8db5b2
vignesh-kv <kv.vignesh2004@gmail.com>
2025-01-29 19:16:41 +0530

    Add files via upload

---
+++ biggerone.java
@@ -0,0 +1,12 @@
+class BiggerOne {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 21;
+        
+        if (a > b) {
+            System.out.println("a is bigger number");
+        } else {
+            System.out.println("b is bigger number");
+        }
+    }
+}
