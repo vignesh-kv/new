d6cef818195e28c98a94b9795489b42982c6457a
vignesh-kv <kv.vignesh2004@gmail.com>
2025-02-11 21:36:56 +0530

    Add files via upload

---
+++ inferio.java
@@ -0,0 +1,22 @@
+class inferio{
+public static void main(String[]args)
+{
+String s="abc";
+switch(s)
+{
+case "abc":
+System.out.print("abcd");
+break;
+case "def":
+System.out.print("efgh");
+break;
+case "ghi":
+System.out.print("ijkl");
+break;
+default:
+System.out.print("ijkl");
+}
+}
+}
+
+
