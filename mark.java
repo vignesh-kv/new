d587b2be62225ec68abef8f6e0b427009f2a6add
vignesh-kv <kv.vignesh2004@gmail.com>
2025-02-24 20:49:27 +0530

    Add files via upload

---
+++ mark.java
@@ -0,0 +1,16 @@
+class mark{
+public static void main(String args[]){
+int i=12;
+int j=14;
+int k=21;
+int l=35;
+int m=45;
+int average=(i+j+k+l+m);
+if(average<35){
+System.out.print("Additional class is required");
+}else{
+System.out.print("you are good to go");
+}
+}
+}
+
