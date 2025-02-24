e0a6f6605c9f3a13c255ad3126bdccf4787ea411
vignesh-kv <kv.vignesh2004@gmail.com>
2025-02-24 20:49:57 +0530

    Add files via upload

---
+++ color.java
@@ -0,0 +1,13 @@
+class color{
+public static void main(String args[]){
+String s1="green";
+if(s1=="red"){
+System.out.print("Stop");
+}
+if(s1=="yellow"){
+System.out.print("get ready");
+}if(s1=="green"){
+System.out.print("Go");
+}
+}
+}
